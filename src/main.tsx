028b841b44361814b09ffef271a908d825086f0b
marleypm16 <yarlempm16@gmail.com>
2025-02-11 12:05:26 -0300

    feat : add global styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { ThemeProvider } from 'styled-components'
-import { GlobalStyled, theme } from './globalStyle.ts'
+import { GlobalStyled, theme } from './styles/globalStyle.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
