8a2149211cb5388c79dde5f5afbf8b9d93857a39
marleypm16 <yarlempm16@gmail.com>
2025-02-11 12:06:49 -0300

    feat : add coffe list component

---
+++ src/components/coffelist/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const CoffeContainer = styled.section`
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: repeat(4, 1fr);
+    gap:20px;
+    align-items: center;  
+    justify-items: center;
+    
+    `
