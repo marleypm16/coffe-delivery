ea70640d5116de1767751837adee0fe4d3fec460
marleypm16 <yarlempm16@gmail.com>
2025-02-12 12:18:03 -0300

    feat : create home component

---
+++ src/components/home/Home.tsx
@@ -0,0 +1,34 @@
+import coffe from '../../assets/Imagem.svg'
+import cartI from '../../assets/icons/Type=shopping-cart-fill.svg'
+import timerI from '../../assets/icons/Type=timer-fill.svg'
+import packageI from '../../assets/icons/Type=package-fill.svg'
+import coffeI from '../../assets/icons/Type=coffee-fill.svg'
+import { Container, Paragraph, ParagraphWithImage, RoundButton, Services, SubTitle, Title } from './styles'
+import { theme } from '../../styles/globalStyle'
+import CoffeList from '../coffelist/CoffeList'
+
+function Home(){
+    return(
+        <>
+        <Container>
+          <div>
+            <Title>Encontre o café perfeito para qualquer hora do dia</Title>
+            <Paragraph>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</Paragraph>
+            <Services>
+              <ParagraphWithImage><RoundButton bgColor = {theme.colors.yellow.dark}><img src={cartI} alt="Compra simples e segura" /></RoundButton>Compra simples e segura</ParagraphWithImage>
+              <ParagraphWithImage><RoundButton bgColor= '#574F4D'><img src={packageI} alt="Embalagem mantém o café intacto" /></RoundButton>Embalagem mantém o café intacto</ParagraphWithImage>
+              <ParagraphWithImage><RoundButton bgColor= {theme.colors.yellow.regular}><img src={timerI} alt="Entrega rápida e rastreada" /></RoundButton>Entrega rápida e rastreada</ParagraphWithImage>
+              <ParagraphWithImage><RoundButton bgColor={theme.colors.purple.regular}><img src={coffeI} alt="Café chega fresquinho até você" /></RoundButton>Café chega fresquinho até você</ParagraphWithImage>
+            </Services>
+          </div>
+          <img src={coffe} alt="Encontre o café perfeito" />
+        </Container>
+        <SubTitle>Nossos Cafés</SubTitle>
+        <CoffeList/>
+        </>
+        
+    )
+        
+}
+
+export default Home;
