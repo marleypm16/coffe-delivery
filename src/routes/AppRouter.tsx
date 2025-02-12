8181ec0e4c45464b2cdee0a7eddf74b7f08a3f00
marleypm16 <yarlempm16@gmail.com>
2025-02-12 12:14:56 -0300

    add router

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "../App";
+import Home from "../components/home/Home";
 import Checkout from "../components/checkout/Checkout";
 
 function AppRouter() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/checkout" element={<Checkout />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/checkout" element={<Checkout />} /> {/* Sem a barra inicial */}
       </Routes>
     </Router>
   );
