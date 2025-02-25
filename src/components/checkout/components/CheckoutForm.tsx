479abc10822331e3b9d786da74e3721904810714
marleypm16 <yarlempm16@gmail.com>
2025-02-25 10:16:09 -0300

    checkout form in progress

--- src/components/checkout/components/CheckoutForm.tsx
+++ src/components/checkout/components/CheckoutForm.tsx
@@ -1,58 +1,98 @@
 import example from '../../../assets/Type=Café Gelado.png';
 import QuantityButton from '../../quantityButton/QuantityButton';
-import pinI from '../../../assets/icons/Type=map-pin-line-regular.svg';
 import Input from '../../input/Input';
-import { DeliveryInformation, FormS } from './styles';
+import MapPin from '../../../assets/icons/Type=map-pin-line-regular.svg'
+import Money from '../../../assets/icons/Type=currenc-dollar-regular.svg'
+import {  Container,Card,Header,InputGroup,Subtitle, FormS, LocalizationAndPaymentSection, OrderSection, PaymentMethod, CartItem } from './styles';
+import CardI from '../../../assets/icons/Type=credit-card-regular.svg'
+import Cash from '../../../assets/icons/Type=money-regular.svg'
 
 function CheckoutForm(){
     return (
         <div>
-            <h2>Complete seu pedido</h2>
+            <h2 >Complete seu pedido</h2>
             <FormS>
-                <section>
-                    <DeliveryInformation>
-                        <div>
-                            <img src={pinI} alt="Digite seu endereço de entrega" />
+                <LocalizationAndPaymentSection>
+                <Container>
+                        <Card>
+                            <Header>
+                            <img src={MapPin} alt="" />
                             <div>
-                                <h3>Endereço de Entrega</h3>
-                                <p>Informe o endereço onde deseja receber seu pedido</p>
+                                <h3 style={{ fontSize: "1rem", fontWeight: "600",margin:0 }}>Endereço de Entrega</h3>
+                                <Subtitle>Informe o endereço onde deseja receber seu pedido</Subtitle>
                             </div>
-                        </div>
-                        <div>
-                            <Input type="number" placeHolder="CEP" />
-                            <Input type="text" placeHolder="Rua" />
-                            <Input type="text" placeHolder="Número" />
-                            <Input type="text"  placeHolder="Complemento"  />
-                            <Input type="text"  placeHolder="Bairro"  />
-                            <Input type="text"  placeHolder="Cidade"  />
-                            <Input type="text"  placeHolder="UF"  />
-                        </div>
-                    </DeliveryInformation>
-                    <div>
-                        <h3>Pagamento</h3>
-                        <p>O pagamento é feito na entrega. Escolha a forma que deseja pagar</p>
-                        <button>Cartão de Crédito</button>
-                        <button>Cartão de Débito</button>
-                        <button>Dinheiro</button>
-                    </div>
-                </section>
-                <section>
+                            </Header>
+                            <Input type='text' placeHolder="CEP" width="33%" />
+                            <Input type='text' placeHolder="Rua" width="100%" />
+                            <InputGroup>
+                            <Input type='text' placeHolder="Número" width="33%" />
+                            <Input type='text' placeHolder="Complemento (Opcional)" width="70%" />
+                            </InputGroup>
+                            <InputGroup>
+                            <Input type='text' placeHolder="Bairro" width="33%" />
+                            <Input type='text' placeHolder="Cidade" width="50%" />
+                            <Input type='text' placeHolder="UF" width="16%" />
+                            </InputGroup>
+                        </Card>
+                    </Container>
+                    <Container>
+                        <Card>
+                            <Header>  
+                                <img src={Money} alt="" />  
+                            
+                                <div>
+                                <h3 style={{margin:0}} >Pagamento</h3>
+                                <Subtitle>O pagamento é feito na entrega. Escolha a forma que deseja pagar</Subtitle>
+                            </div>
+                            </Header>
+                            <div style={{display: "flex", justifyContent: "space-around"}}>
+                                <PaymentMethod>
+                                    <img src={CardI} alt="" />
+                                    Cartão de Crédito
+                                    </PaymentMethod>
+                                <PaymentMethod>
+                                    <img src={CardI} alt="" />
+                                    Cartão de Débito</PaymentMethod>
+                                <PaymentMethod>
+                                    <img src={Cash} alt="" />
+                                    Dinheiro</PaymentMethod>
+                            </div>
+                           
+                        </Card>
+                    </Container>
+                    
+                </LocalizationAndPaymentSection>
+                <OrderSection>
                     <h1>Cafés Selecionados</h1>
-                    <div>
-                        <img src={example} alt="Café de exemplo" />
-                        <h2>Nome do Café</h2>
-                        <QuantityButton />
-                        <button>Remove</button>
-                        <span>R$ 9.99</span>
-                    </div>
-                    <div>
-                        <p>Total de itens R$29,9</p>
-                        <p>Total de entrega R$3.5</p>
-                        <h3>TOTAL - R$34</h3>
-                    </div>
-                    <button type="submit">Confirmar Pedido</button>
 
-                </section>
+                    <Container>
+                        <CartItem>
+                            <div style={{display: "flex",gap: "16px",alignItems: "center"}} >
+                                <img src={example} alt="Café de exemplo" />
+                                <div >
+                                    <h2>Expresso Tradicional</h2>
+                                    <div style={{display: "flex",gap: "16px",alignItems: "center"}}>
+                                        <QuantityButton />
+                                        <button>Remove</button>
+                                    </div>
+                                </div>
+                            </div>
+                           
+                           
+                            <span>R$ 9.99</span>
+                        </CartItem>
+                        <div style={{width:"100%",backgroundColor:"lightgrey",height:"1px"}}></div>
+
+                        <div>
+                            <p>Total de itens R$29,9</p>
+                            <p>Total de entrega R$3.5</p>
+                            <h3>TOTAL - R$34</h3>
+                        </div>
+                        <button type="submit">Confirmar Pedido</button>
+                    </Container>
+                    
+
+                </OrderSection>
             </FormS>
         </div>
     );
--- src/components/checkout/components/styles.ts
+++ src/components/checkout/components/styles.ts
@@ -1,20 +1,83 @@
 import styled from "styled-components";
+import { theme } from "../../../styles/globalStyle";
 
 export const FormS = styled.form`
     display: flex;
-    justify-content: space-between;
-    align-items: center;
+    gap: 24px;
 
 `;
+export const LocalizationAndPaymentSection = styled.section`
+    flex: 1;
+`;
 
-export const DeliveryInformation = styled.section`
-    width: 50%;
+export const OrderSection = styled.section`
+    width: 35%;
+`;
+export const Container = styled.div`
+  background: #F3F2F2;
+  padding: 24px;
+  border-radius: 8px;
+  max-width: 640px;
+  margin-bottom: 24px;
+`;
+
+export const Card = styled.div`
+  padding: 24px;
+  border-radius: 8px;
+`;
+
+export const Header = styled.div`
+  display: flex;
+  align-items: flex-start;
+  gap: 8px;
+  margin-bottom: 16px;
+`;
+
+export const Title = styled.h2`
+  font-size: 1.125rem;
+  font-weight: 600;
+  margin-bottom: 16px;
+`;
+
+export const Subtitle = styled.p`
+  font-size: 0.875rem;
+  color: #737373;
+`;
+
+export const InputGroup = styled.div`
+  display: flex;
+  gap: 8px;
+`;
+
+export const PaymentMethod = styled.button`
+    background-color: #E6E5E5;
+    padding: 12px;
     display: flex;
-    flex-direction: column;
+    align-items: center;
+    gap: 8px;
+    border-radius: 8px;
+    border:none;
+    font-size: ${theme.fontSIzes.roboto.s};
+    font-family:${theme.font.roboto};
+`
+
+export const CartItem = styled.div`
+    display: flex;
+    gap: 16px;
     align-items: center;
     justify-content: space-between;
-    background-color: #f5f5f5;
-    padding: 10px;
-    border-radius: 10px;
-
-`
+    margin-bottom: 16px;
+    img {
+        width: 64px;
+        height: 64px;
+        border-radius: 8px;
+    }
+    h2 {
+        font-size: 1.125rem;
+        font-weight: 600;
+    }
+    span {
+        font-size: 1.125rem;
+        font-weight: 600;
+    }
+`;
